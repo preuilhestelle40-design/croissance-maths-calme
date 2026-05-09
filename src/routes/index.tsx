a10f708540ea782e510b3dd76f22f12089305838
preuilhestelle40-design <preuilhestelle40@gmail.com>
2026-05-09 19:45:14 +0200

    Updated index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Nav } from "@/components/site/Nav";
 import { Footer } from "@/components/site/Footer";
 import heroImg from "@/assets/hero-math.jpg";
-import estelleImg from "@/assets/estelle-portrait.jpg";
+import estelleImg from "@/assets/img_1324_2.jpg";
 
 export const Route = createFileRoute("/")({
   head: () => ({
